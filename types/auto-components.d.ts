d33a4b072766381d6a002b4f945b7cb9fe465e76
zhongjunwei <liwenxin@wisight.cn>
2022-07-05 10:44:08 +0800

    all update config

--- types/auto-components.d.ts
+++ types/auto-components.d.ts
@@ -3,14 +3,18 @@
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
 declare module 'vue' {
-  export interface GlobalComponents {
-    IconCommunity: typeof import('./../src/components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./../src/components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./../src/components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./../src/components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./../src/components/icons/IconTooling.vue')['default']
-    TabBar: typeof import('./../src/components/TabBar.vue')['default']
-  }
+	export interface GlobalComponents {
+		IconCommunity: typeof import('./../src/components/icons/IconCommunity.vue')['default']
+		IconDocumentation: typeof import('./../src/components/icons/IconDocumentation.vue')['default']
+		IconEcosystem: typeof import('./../src/components/icons/IconEcosystem.vue')['default']
+		IconSupport: typeof import('./../src/components/icons/IconSupport.vue')['default']
+		IconTooling: typeof import('./../src/components/icons/IconTooling.vue')['default']
+		TabBar: typeof import('./../src/components/TabBar.vue')['default']
+		VanCell: typeof import('vant/es')['Cell']
+		VanImage: typeof import('vant/es')['Image']
+		VanTabbar: typeof import('vant/es')['Tabbar']
+		VanTabbarItem: typeof import('vant/es')['TabbarItem']
+	}
 }
 
-export { }
+export {}
