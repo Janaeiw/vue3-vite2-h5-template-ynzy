777405172acbbae3472555ffe5bf41d87cd48dba
Janaeiw <40946510+Janaeiw@users.noreply.github.com>
2022-07-20 08:12:48 +0000

    Deploying to gh-pages from  @ fa25c8a5443f0c9ea651a0ed82c3273abcf3a56b 🚀

--- static/js/authController-7ed70f54.js
+++
@@ -1,1 +0,0 @@
-import{P as r}from"./index-17906d8b.js";const t=()=>r({url:"/user/info",method:"get",loading:!0});function n(e){return r({url:"/login",method:"POST",data:e,loading:!0})}export{t as f,n as l};
--- static/js/demo-844fbd76.js
+++
@@ -1,1 +0,0 @@
-import{d as e,k as t,q as r}from"./index-17906d8b.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
