1051c3b4d86ad2373f8ce3784c8ec1a05bddc92a
Janaeiw <40946510+Janaeiw@users.noreply.github.com>
2022-07-27 03:22:03 +0000

    Deploying to gh-pages from  @ 14ace15f89c02a901143ee7bd1ee9924a33c40a2 🚀

--- static/js/demo-a4de50e1.js
+++
@@ -1,1 +0,0 @@
-import{d as e,k as t,q as r}from"./index-d78fa6c6.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
---
+++ static/js/testCssModel-0147eb2b.js
@@ -0,0 +1,1 @@
+import{d as e,k as o,q as l}from"./index-2c4acbfe.js";const a="_moduleClass_15i7z_1";var s={moduleClass:a},d=e({setup(){return console.log("css--model",s),()=>o("div",{class:`root ${s.moduleClass}`},[l("\u6D4B\u8BD5css-modules")])}});export{d as default};
