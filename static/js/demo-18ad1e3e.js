06e986e5abd6cb6d7cbd827076c1c871894d2130
Janaeiw <40946510+Janaeiw@users.noreply.github.com>
2023-03-15 03:45:36 +0000

    Deploying to gh-pages from  @ 2e2b861f86d29c0f9b982e79d23d641acaefaa5a 🚀

--- static/js/demo-18ad1e3e.js
+++
@@ -1,1 +0,0 @@
-import{d as e,k as t,q as r}from"./index-3de2b923.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
---
+++ static/js/demo-81d6f1a5.js
@@ -0,0 +1,1 @@
+import{d as e,k as t,q as r}from"./index-b48203dc.js";var a=e({setup(){return()=>t("div",null,[r("hello tsx")])}});export{a as default};
